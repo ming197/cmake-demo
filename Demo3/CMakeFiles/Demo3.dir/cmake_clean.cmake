69f643a7761105760c0b17c36fa1f282b2fa1752
hmmei <hmmei@freewheel.tv>
2024-05-06 00:16:27 +0800

    use static linked library to deal with sub_directory problems

---
+++ Demo3/CMakeFiles/Demo3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Demo3.dir/main.cc.o"
+  "CMakeFiles/Demo3.dir/main.cc.o.d"
+  "Demo3"
+  "Demo3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Demo3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
