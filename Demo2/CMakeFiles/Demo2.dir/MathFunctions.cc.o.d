9ae8974bd491a9a2389f7b1a247a58c3560fca67
hmmei <hmmei@freewheel.tv>
2024-05-05 23:01:26 +0800

    one directory and multiple files, use aux_source_directory

---
+++ Demo2/CMakeFiles/Demo2.dir/MathFunctions.cc.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/Demo2.dir/MathFunctions.cc.o: \
+  /Users/hmmei/cmake-demo/Demo2/MathFunctions.cc
---
+++ Demo2/CMakeFiles/Demo2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Demo2.dir/MathFunctions.cc.o"
+  "CMakeFiles/Demo2.dir/MathFunctions.cc.o.d"
+  "CMakeFiles/Demo2.dir/main.cc.o"
+  "CMakeFiles/Demo2.dir/main.cc.o.d"
+  "Demo2"
+  "Demo2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Demo2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
